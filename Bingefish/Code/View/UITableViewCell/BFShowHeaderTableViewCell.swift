0491fa42ed3b585386fac601c243b4a759b2fe87
Genki Mine <gomfucius@gmail.com>
2016-08-02 21:44:32 -0700

    Add gradient to header view

--- Bingefish/Code/View/UITableViewCell/BFShowHeaderTableViewCell.swift
+++ Bingefish/Code/View/UITableViewCell/BFShowHeaderTableViewCell.swift
@@ -16,10 +16,24 @@ class BFShowHeaderTableViewCell: UITableViewCell
     @IBOutlet weak var episodesLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
     
+    var gradientLayer: CAGradientLayer?
+    
     override func awakeFromNib()
     {
         super.awakeFromNib()
         // Initialization code
+        
+        if gradientLayer == nil {
+            let gradient: CAGradientLayer = CAGradientLayer()
+            let startColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5)
+            let endColor = UIColor(white: 0.0, alpha: 0.0)
+            gradient.colors = [startColor.CGColor, endColor.CGColor]
+            gradient.locations = [0.0 , 1.0]
+            gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
+            gradient.endPoint = CGPoint(x: 0.8, y: 1.0)
+            gradient.frame = CGRect(x: 0.0, y: 0.0, width: self.contentView.frame.size.width, height: self.contentView.frame.size.height)
+            headerImageView.layer.insertSublayer(gradient, atIndex: 0)
+        }
     }
 
     override func setSelected(selected: Bool, animated: Bool) 
